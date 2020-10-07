a733de05dcd0b18d2db27fb7a9714960f354a9af
gigiromero <giselleromeroxv@gmail.com>
2020-10-06 20:31:53 -0500

    Added Smart Save text and styled it

--- src/smartsave.py
+++ src/smartsave.py
@@ -27,6 +27,12 @@ class SmartSaveUI(QtWidgets.QDialog):
                             QtCore.Qt.WindowContextHelpButtonHint)
         self.create_ui()
 
+    def create_ui(self):
+        self.title_lbl = QtWidgets.QLabel("Smart Save")
+        self.title_lbl.setStyleSheet("font: bold 20px")
+        self.main_lay = QtWidgets.QVBoxLayout()
+        self.main_lay.addWidget(self.title_lbl)
+        self.setLayout(self.main_lay)
 
 class SceneFile(object):
     """An abstract representation of a SceneFile"""
